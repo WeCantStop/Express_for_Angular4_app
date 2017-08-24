7578f49f0914298d73dbc0e593f38c84a5043494
Will <pan123033@qq.com>
2017-08-24 15:00:39 +0800

    完成mongo对接

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ var users = require('./routes/users');
 
 var app = express();
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    //res.header("X-Powered-By",' 3.2.1')
-    // res.header("Content-Type", "application/json;charset=utf-8");
-    next();
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  //res.header("X-Powered-By",' 3.2.1')
+  // res.header("Content-Type", "application/json;charset=utf-8");
+  next();
 });
 
 // view engine setup
@@ -27,7 +27,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,14 +37,14 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
+// 连接 mongodb数据库
+var MongoClient = require('mongodb').MongoClient;
+// 连接到的数据库
+var DB_CONN_STR = 'mongodb://localhost:27017/server';
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('halo');
 });
 
+// test data
 const resData = {
     code: 0,
     data: {
@@ -18,6 +25,13 @@ const resData = {
     success: true
 };
 
+let resStatus = {
+    code: 0,
+    status: 'ok',
+    success: true,
+    message: '操作成功'
+}
+
 /** /users/queryDuoDuo  get **/
 router.post('/queryDuoDuo', function (req, res, next) {
     res.send(resData);
@@ -27,5 +41,56 @@ router.post('/queryDuoDuo', function (req, res, next) {
 router.get('/age', function (req, res, next) {
     res.send(resData);
 });
+
+/** /users/addUser  post */
+router.post('/addUser', function (req, res, next) {
+    // 此处要连接 mongo 数据库insert数据
+    req = req.body;
+    MongoClient.connect(DB_CONN_STR, function (err, db) {
+        insertData(db, req, function (result) {
+            // 返回的操作 mongodb的数据
+            console.log(result);
+            db.close();
+        });
+    });
+    res.send(resStatus);
+})
+
+/** /users/getUsers  get */
+router.get('/getUser', function (req, res, next) {
+    MongoClient.connect(DB_CONN_STR, function (err, db) {
+        queryUsers(db, function (result) {
+            // 返回数据
+            res.status(200);
+            res.json(result);
+            db.close();
+        })
+    });
+})
+
+var insertData = function (db, req, callback) {
+    // 定义表 site
+    var collection = db.collection('user_base');
+    // 表的操作 insert
+    collection.insert(req, function (err, result) {
+        if (err) {
+            console.log('Error:' + err);
+            return;
+        }
+        callback(result);
+    });
+}
+
+var queryUsers = function (db, callback) {
+    // 定义表 site
+    var collection = db.collection('user_base');
+    collection.find({}).toArray(function (err, result) {
+        if (err) {
+            console.log('Error:' + err);
+            return;
+        }
+        callback(result);
+    })
+}
 
 module.exports = router;
