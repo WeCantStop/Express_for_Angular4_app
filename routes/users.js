c2b7a87d7c5bb2cfc66d74d502dac692e482e8ee
Will <pan123033@qq.com>
2017-08-24 22:04:14 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var router = express.Router();
 // 连接 mongodb数据库
 var MongoClient = require('mongodb').MongoClient;
 // 连接到的数据库
-var DB_CONN_STR = 'mongodb://localhost:27017/server';
+var DB_CONN_STR = 'mongodb://localhost:27017/data';
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -62,8 +62,10 @@ router.get('/getUser', function(req, res, next) {
     MongoClient.connect(DB_CONN_STR, function(err, db) {
         queryUsers(db, function(result) {
             // 返回数据
-            res.status(200);
-            res.json(result);
+            // res.status(200);
+            // res.json(result);
+            resStatus.data = result;
+            res.send(resStatus);
             db.close();
         })
     });
